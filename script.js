5002dbd97916be01fa2fd65e0d6e176102fd0bd8
Elaine-Vargas <elainevargasreyes@gmail.com>
2024-09-28 17:39:47 -0400

    mostrar/ocultar
    creacion de botones en la tarejta

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function muestra_oculta(id){
+function muestra_oculta(id) {
     let div = document.getElementById(id);
-    if(div.style.display=="none"){
-        div.style.display="flex";
+    if (div.style.display == "none") {
+      div.style.display = "block"; // Mostrar el contenido
+    } else {
+      div.style.display = "none"; // Ocultar el contenido
     }
-    else{
-        div.style.display="none";
-    }}
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+    var botones = document.querySelectorAll('.oculta-muestra button');
+    
+    botones.forEach(function(boton) {
+        boton.addEventListener('click', function() {
+            if(this.textContent === 'ver más') {
+                this.textContent = 'ver menos';
+            } else if (this.textContent === 'ver menos') {
+                this.textContent = 'ver más';
+            }
+        });
+    });
+});
